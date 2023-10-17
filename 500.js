ff8a793c596b9c9e0b5ec3c38eccafcc9312afd6
apoorvapendse <apoorvavpendse@gmail.com>
2023-10-17 14:13:20 +0530

    500 reqs in one hour

--- 500.js
+++ 500.js
@@ -2,17 +2,17 @@
 
 let timeForOneRequest = (3600 * 1000) / 500;
 let count = 0;
-console.log("time interval for every req:", timeForOneRequest);
+console.log(`time interval for every req:${timeForOneRequest}ms`);
 let interval = setInterval(async () => {
   try {
-    console.log("sending request");
+    console.log("sending request number:", count + 1);
     const resp = await fetch("https://secureninja.onrender.com");
     const data = await resp.json();
     console.log(data);
   } catch (err) {
     console.log(err);
   }
-  if (count ==500) {
+  if (count == 500) {
     clearInterval(interval);
   }
   count++;
